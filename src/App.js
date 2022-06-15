362be51601b788d800ecbde8c84f3372ccc24bc6
Cheol-Cheol <dream174822@gmail.com>
2022-06-15 17:23:33 +0900

    ✨ Feat: Add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 /*eslint-disable*/
-import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Hello World!!</h1>
+      <Button>버튼!</Button>
     </div>
   );
 }
