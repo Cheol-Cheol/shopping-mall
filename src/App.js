f5617116f403b78274651e06ea7f0ed3c3d7351e
Cheol-Cheol <dream174822@gmail.com>
2022-06-16 16:46:19 +0900

    💄 Design: Add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 /*eslint-disable*/
 import "./App.css";
-import { Button } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!!</h1>
-      <Button>버튼!</Button>
+      {/* TODO: router 설정 시 link 달아줘야 함 */}
+      <h1 className="site-name">CHEOL CHEOL</h1>
+      {/* FIXME: NavBar */}
+      <Nav className="justify-content-center" activeKey="best">
+        <Nav.Item>
+          {/* TODO: Nav.Link - router 설정 시 router로 관리해야 함 */}
+          <Nav.Link eventKey="best" href="/" className="nav">
+            BEST
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/outwear" className="nav">
+            OUTWEAR
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/top" className="nav">
+            TOP
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/bottom" className="nav">
+            BOTTOM
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/user" className="nav">
+            <i class="fa-regular fa-user"></i>
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/cart" className="nav">
+            <i class="fa-solid fa-cart-shopping"></i>
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/search" className="nav">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
     </div>
   );
 }
