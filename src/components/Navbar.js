0736a5fec1d172c921ddbbb58d45f89b1482b28e
Cheol-Cheol <dream174822@gmail.com>
2022-06-16 17:36:34 +0900

    🔨 Chore: Add Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+
+function Navbar() {
+  return (
+    <Nav className="justify-content-center" activeKey="best">
+      <Nav.Item>
+        {/* TODO: Nav.Link - router 설정 시 router로 관리해야 함 */}
+        <Nav.Link eventKey="best" href="/" className="nav">
+          BEST
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/outwear" className="nav">
+          OUTWEAR
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/top" className="nav">
+          TOP
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/bottom" className="nav">
+          BOTTOM
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/user" className="nav">
+          <i class="fa-regular fa-user"></i>
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/cart" className="nav">
+          <i class="fa-solid fa-cart-shopping"></i>
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/search" className="nav">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
+}
+
+export default Navbar;
