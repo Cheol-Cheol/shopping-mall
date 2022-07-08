648596cd765a25339a822eb7494ec73784fa318e
Cheol-Cheol <dream174822@gmail.com>
2022-07-08 20:54:21 +0900

    📝 Chore: Add dummy data

---
+++ src/data.js
@@ -0,0 +1,26 @@
+// 임의로 서버에서 호출한 데이터 가정
+let data = [
+  {
+    id: 0,
+    title: "nylon half hoodie anorak (4color)",
+    price: 41000,
+  },
+  {
+    id: 1,
+    title: "sand nylon windbreaker (2color)",
+    price: 76000,
+  },
+  {
+    id: 2,
+    title: `(기획) neck collar windbreaker (5color)
+    `,
+    price: 36000,
+  },
+  {
+    id: 3,
+    title: `nelson pin-tuck wide pants (GRAY)`,
+    price: 44000,
+  },
+];
+
+export default data;
